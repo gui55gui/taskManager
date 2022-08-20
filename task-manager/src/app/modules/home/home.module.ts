a9ed34b233789f1d9251475b971752c08eea625d
guilherme.machado <dev03@consulting.allog.com.br>
2022-08-20 14:38:01 -0300

    Desenvolvimento da estrutura base.

---
+++ task-manager/src/app/modules/home/home.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Components
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonDeleteAllComponent } from './components/button-delete-all/button-delete-all.component';
+import { InputAddItemsComponent } from './components/input-add-items/input-add-items.component';
+import { ListComponent } from './components/list/list.component';
+
+//Page
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    ButtonDeleteAllComponent,
+    InputAddItemsComponent,
+    ListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
